8c4a4affc17b6ed6797d91928313fec0f7110c70
MatthewDerzay <mderzay@wisc.edu>
2019-02-19 09:31:01 +0100

    Added the player and scripts

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 input;
+
+    float speed = 5;
+    Rigidbody2D rb2d = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb2d.velocity;
+
+        velocity.x = input.x * speed;
+
+        rb2d.velocity = velocity;
+    }
+}
