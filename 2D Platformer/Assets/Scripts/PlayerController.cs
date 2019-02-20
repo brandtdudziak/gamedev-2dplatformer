fdc52694fcf53eb9303c2017d5fe13de6105c954
MatthewDerzay <mderzay@wisc.edu>
2019-02-20 15:55:59 +0100

    Walljump

--- 2D Platformer/Assets/Scripts/PlayerController.cs
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jump;
+    public float jumpPush;
+    public float wallJumpDelay;
+    float timerWJ;
     float moveVelocity;
-    bool isGrounded = false;
     public LayerMask playerMask;
+    bool isGrounded = false;
+    bool isOnWallLeft = false;
+    bool isOnWallRight = false;
+    bool wallJump = false;
 
     Transform tagGround;
+    Transform tagWallRight;
+    Transform tagWallLeft;
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -18,21 +26,37 @@ public class PlayerController : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         tagGround = GameObject.Find(this.name + "/tag_ground").transform;
+        tagWallRight = GameObject.Find(this.name + "/tag_wallright").transform;
+        tagWallLeft = GameObject.Find(this.name + "/tag_wallleft").transform;
+        timerWJ = wallJumpDelay;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        isGrounded = Physics2D.Linecast(transform.position, tagGround.position, playerMask);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        Jump();
+        if (!wallJump)
         {
-            if (isGrounded)
+            Move();
+        }
+        else
+        {
+            if(timerWJ < 0)
             {
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jump);
+                wallJump = false;
+                timerWJ = wallJumpDelay;
+            }
+            else
+            {
+                timerWJ -= Time.deltaTime;
             }
         }
 
+        
+    }
+
+    public void Move()
+    {
         moveVelocity = 0;
 
         if (Input.GetKey(KeyCode.A))
@@ -45,5 +69,32 @@ public class PlayerController : MonoBehaviour
         }
 
         rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+    }
+
+    public void Jump()
+    {
+        isGrounded = Physics2D.Linecast(transform.position, tagGround.position, playerMask);
+        isOnWallLeft = Physics2D.Linecast(transform.position, tagWallLeft.position, playerMask);
+        isOnWallRight = Physics2D.Linecast(transform.position, tagWallRight.position, playerMask);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jump);
+            }
+
+            if (isOnWallLeft && !isGrounded)
+            {
+                rb2d.velocity = new Vector2(jumpPush, jump);
+                wallJump = true;
+            }
+
+            if (isOnWallRight && !isGrounded)
+            {
+                rb2d.velocity = new Vector2(-jumpPush, jump);
+                wallJump = true;
+            }
+        }
     }
 }
