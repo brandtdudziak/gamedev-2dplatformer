e623f53510474e72a6b773701771c480ef217666
Brandt <brandtdudziak@gmail.com>
2019-02-20 15:26:27 +0100

    Game manager and next scene

--- 2D Platformer/Assets/Scripts/Player.cs
+++ 2D Platformer/Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject spawnPoint;
+    private GameObject gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.CompareTag("Fall"))
+        {
+            SpawnPoint sp = spawnPoint.GetComponent<SpawnPoint>();
+            sp.Respawn(gameObject);
+        }
+        if(collider.gameObject.CompareTag("Goal"))
+        {
+            GameManager gm = gameManager.GetComponent<GameManager>();
+            gm.NextScene();
+        }
     }
 }
