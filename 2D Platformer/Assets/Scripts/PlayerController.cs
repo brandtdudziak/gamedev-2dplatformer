a2cefe0a378e15321101985f63bf603196c87eab
MatthewDerzay <mderzay@wisc.edu>
2019-02-20 13:24:05 +0100

    Revert "Revert "Fixed wall jumps""
    This reverts commit 2ed8562eadba640f3ea06c67889b0a26a7a9b3af.

--- 2D Platformer/Assets/Scripts/PlayerController.cs
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -7,24 +7,29 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jump;
     float moveVelocity;
-    Rigidbody2D rb2d = null;
-    bool isGrounded = true;
+    bool isGrounded = false;
+    public LayerMask playerMask;
+
+    Transform tagGround;
+    Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        tagGround = GameObject.Find(this.name + "/tag_ground").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        isGrounded = Physics2D.Linecast(transform.position, tagGround.position, playerMask);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
             {
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jump);
-                isGrounded = false;
             }
         }
 
@@ -40,10 +45,5 @@ public class PlayerController : MonoBehaviour
         }
 
         rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
-    }
-
-    void OnCollisionEnter2D()
-    {
-        isGrounded = true;
     }
 }
